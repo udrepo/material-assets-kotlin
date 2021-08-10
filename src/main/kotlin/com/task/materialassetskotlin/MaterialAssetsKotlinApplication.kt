bc78a1b3149bf13b81f6ee8dd89fe90cf9d9fec1
ulan <ulan.0418@gmail.com>
2021-08-10 16:32:44 +0600

    initial commit

---
+++ src/main/kotlin/com/task/materialassetskotlin/MaterialAssetsKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.task.materialassetskotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MaterialAssetsKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<MaterialAssetsKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/task/materialassetskotlin/MaterialAssetsKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.task.materialassetskotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MaterialAssetsKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
